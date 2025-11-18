7dc9380c0cf0e2e9ff0bda31a683d419a83989eb
Eduardo Klosowski <eduardo.klosowski@contaswap.com>
2025-11-18 14:03:39 +0000

    Configure devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xe
+
+# Config uv
+pipx install uv==0.9.10
+[ -e .venv ] || (uv venv --python /usr/local/bin/python .venv && uv sync --locked)
+
+# Completion
+pipx install argcomplete
+mkdir -p ~/.local/share/bash-completion/completions
+echo 'eval "$(pip completion --bash)"' > ~/.local/share/bash-completion/completions/pip
+echo 'eval "$(register-python-argcomplete pipx)"' > ~/.local/share/bash-completion/completions/pipx
